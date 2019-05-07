ffded413f9b5e188dbec068b511622af4bbb0a59
we <immrishabh@gmail.com>
2019-05-07 23:12:36 +0530

    Threads Implementation Added

--- app/src/main/java/com/nitrr/ecell/esummit/ecellapp/activities/MainActivity.java
+++ app/src/main/java/com/nitrr/ecell/esummit/ecellapp/activities/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nitrr.ecell.esummit.ecellapp.R;
+import com.nitrr.ecell.esummit.ecellapp.misc.threads.Priority;
+import com.nitrr.ecell.esummit.ecellapp.misc.threads.PriorityRunnable;
+import com.nitrr.ecell.esummit.ecellapp.misc.threads.ThreadPoolExecutorSupplier;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //performBackgroundTasks();
+    }
+
+    private void performBackgroundTasks() {
+        ThreadPoolExecutorSupplier.getInstance().getDoBackgroundTasksThreadPoolExecutor().submit(new PriorityRunnable(Priority.HIGH) {
+            @Override
+            public void run() {
+                // Perform task here.
+            }
+        });
     }
 }
