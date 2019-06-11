f44d60f04bef9774c8491356ad5303bb7ff8c470
Viren Khatri <virenk2906@gmail.com>
2019-06-11 08:30:59 +0530

    Recycler View, BottomSheet Navigation in Home Activity

--- app/src/main/java/com/nitrr/ecell/esummit/ecellapp/misc/MySnapHelper.java
+++ app/src/main/java/com/nitrr/ecell/esummit/ecellapp/misc/MySnapHelper.java
@@ -9,6 +9,10 @@ import android.view.View;
 
 public class MySnapHelper extends SnapHelper {
 
+    private boolean run = false;
+    @Nullable
+    private OrientationHelper mHorizontalHelper;
+
     @Override
     public void attachToRecyclerView(@Nullable RecyclerView recyclerView) throws IllegalStateException {
         super.attachToRecyclerView(recyclerView);
@@ -41,13 +45,83 @@ public class MySnapHelper extends SnapHelper {
     @Nullable
     @Override
     public View findSnapView(RecyclerView.LayoutManager layoutManager) {
-        return null;
+        return this.findCenterView(layoutManager, this.getHorizontalHelper(layoutManager));
     }
 
     @Override
-    public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int i, int i1) {
-        return 0;
+    public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velX, int velY) {
+        int currentPos = layoutManager.getPosition(this.findSnapView(layoutManager));
+        int max = layoutManager.getChildCount();
+        int targetPos = currentPos;
+
+        if(run) {
+            if(currentPos == max - 1) {
+                if(velX > 0) {
+                    targetPos = max - 1;
+                }
+            }
+
+            else if(currentPos == 0) {
+                if(velX < 0) {
+                    targetPos = 1;
+                }
+            }
+
+            else {
+                if(velX > 0) {
+                    targetPos = currentPos - 1;
+                }
+                else if(velX < 0) {
+                    targetPos = currentPos + 1;
+                }
+
+            }
+            run = false;
+        }
+        if(velX == 0)
+            run = true;
+
+        return targetPos;
     }
 
+    @Nullable
+    private View findCenterView(RecyclerView.LayoutManager layoutManager, OrientationHelper helper) {
+        int childCount = layoutManager.getChildCount();
+        if (childCount == 0) {
+            return null;
+        } else {
+            View closestChild = null;
+            int center;
+            if (layoutManager.getClipToPadding()) {
+                center = helper.getStartAfterPadding() + helper.getTotalSpace() / 2;
+            } else {
+                center = helper.getEnd() / 2;
+            }
 
+            int absClosest = 2147483647;
+
+            for(int i = 0; i < childCount; ++i) {
+                View child = layoutManager.getChildAt(i);
+                int childCenter = helper.getDecoratedStart(child) + helper.getDecoratedMeasurement(child) / 2;
+                int absDistance = Math.abs(childCenter - center);
+                if (absDistance < absClosest) {
+                    absClosest = absDistance;
+                    closestChild = child;
+                }
+            }
+
+            return closestChild;
+        }
+    }
+
+    @NonNull
+    private OrientationHelper getHorizontalHelper(@NonNull RecyclerView.LayoutManager layoutManager) {
+        if (this.mHorizontalHelper == null /*|| this.mHorizontalHelper.mLayoutManager != layoutManager*/) {
+            this.mHorizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager);
+        }
+
+        return this.mHorizontalHelper;
+    }
+    //The commented region in the () above showing an error. the error is not shown in LinearSnapHelper.java class where the
+    //same method is there.
 }
