a99963449d37b675cc6659a7ae0140cbc9255e4e
Viren Khatri <virenk2906@gmail.com>
2019-06-19 12:10:41 +0530

    Basic Layout

--- app/src/main/java/com/nitrr/ecell/esummit/ecellapp/activities/ESummitActivity.java
+++ app/src/main/java/com/nitrr/ecell/esummit/ecellapp/activities/ESummitActivity.java
@@ -2,13 +2,25 @@ package com.nitrr.ecell.esummit.ecellapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ScrollView;
+
 import com.nitrr.ecell.esummit.ecellapp.R;
 
 public class ESummitActivity extends AppCompatActivity {
+
+    ScrollView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esummit);
+
+        view = findViewById(R.id.view);
+        initialize();
     }
+
+    public void initialize() {
+
+    }
+
 }
