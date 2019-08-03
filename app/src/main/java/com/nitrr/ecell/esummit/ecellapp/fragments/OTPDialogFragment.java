58f7e6d40aaaeb511a4e9a0a1516f29f412e0d45
Rishabh <immrishabh@gmail.com>
2019-08-03 14:56:13 +0530

    Pull

--- app/src/main/java/com/nitrr/ecell/esummit/ecellapp/fragments/OTPDialogFragment.java
+++ app/src/main/java/com/nitrr/ecell/esummit/ecellapp/fragments/OTPDialogFragment.java
@@ -1,6 +1,5 @@
 package com.nitrr.ecell.esummit.ecellapp.fragments;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
